3e3b7272df395717806060dd49f1c65ed0b61db4
lila@lila.com <git@git.com>
2013-05-03 23:30:18 -0400

    actualizacion mienmbros comite

--- src/com/py/sap/adm/miembrosComite/MiembrosComiteFormulario.py
+++ src/com/py/sap/adm/miembrosComite/MiembrosComiteFormulario.py
@@ -1,5 +1,5 @@
-from wtforms import Form, TextField, validators, IntegerField, SelectField, DateField
-from com.py.sap.adm.mod.Proyecto import Proyecto
+from wtforms import Form, TextField, validators, IntegerField, SelectField, DateField, PasswordField
+from com.py.sap.adm.mod.Usuario import Usuario
 from com.py.sap.util.database import engine
 from sqlalchemy.orm import scoped_session, sessionmaker
 
@@ -7,12 +7,11 @@ db_session = scoped_session(sessionmaker(autocommit=False,
                                        autoflush=False,
                                        bind=engine))
 
-class FaseFormulario(Form):
-    nro_orden = IntegerField('Numero Orden', [validators.Required()]) 
-    nombre = TextField('Nombre', [validators.Length(min=2, max=50), validators.Required()])
-    descripcion = TextField('Descripcion', [validators.Length(min=2, max=100), validators.Required()])
-    estado = SelectField('Estado', choices=[('I', 'Inicial'), ('P', 'En Progreso'), ('L', 'En Linea Base'), ('A', 'Aprobado')])
-    fecha_inicio = DateField('Fecha Inicio', format='%Y-%m-%d')
-    fecha_fin = DateField('Fecha Fin', format='%Y-%m-%d' )
-    id_proyecto = TextField('Proyecto')
-#,coerce=int
+class MiembrosComiteFormulario(Form):
+    usuario = TextField('Usuario', [validators.Length(min=2, max=50), validators.Required()])
+    nombre = TextField('Nombre', [validators.Length(min=2, max=100), validators.Required()])
+    apellido = TextField('Apellido', [validators.Length(min=2, max=100), validators.Required()])
+    correo = TextField('Correo', [validators.Length(min=2, max=100), validators.Required(), validators.Email()])
+    domicilio = TextField('Domicilio', [validators.Length(min=2, max=150), validators.Required()])
+    telefono = TextField('Telefono', [validators.Length(min=2, max=50), validators.Required()])
+    fecha_nac = DateField('Fecha Nacimiento', format='%Y-%m-%d' )
--- src/com/py/sap/adm/miembrosComite/__init__.py
+++ src/com/py/sap/adm/miembrosComite/__init__.py
@@ -7,7 +7,7 @@ from flask import Flask, render_template, request, redirect, url_for, flash, ses
 from com.py.sap.adm.mod.Proyecto import Proyecto
 from com.py.sap.adm.mod.Usuario import Usuario
 from com.py.sap.adm.mod.MiembrosComite import MiembrosComite
-from com.py.sap.adm.usuario.UsuarioFormulario import UsuarioFormulario
+from com.py.sap.adm.miembrosComite.MiembrosComiteFormulario import MiembrosComiteFormulario
 import flask, flask.views
 import os
 import datetime
@@ -33,7 +33,7 @@ def nuevomiembrosComite():
     init_db(db_session)
     pro = db_session.query(Proyecto).filter_by(id=session['pry']).first()
     u = db_session.query(Usuario).filter_by(usuario=request.args.get('usu')).first()  
-    form = UsuarioFormulario(request.form,u)
+    form = MiembrosComiteFormulario(request.form,u)
     usuario = db_session.query(Usuario).filter_by(usuario=form.usuario.data).first()  
     if pro.estado != 'N' :
         flash('No se pueden asignar Miembros al Comite de Cambios','info')
@@ -53,34 +53,6 @@ def nuevomiembrosComite():
         flash_errors(form)  
     return render_template('miembrosComite/nuevomiembrosComite.html', form=form)
 
-#@app.route('/miembrosComite/editarmiembrosComite', methods=['GET', 'POST'])
-#def editarmiembrosComite():
-#    init_db(db_session)
-#    pro = db_session.query(Proyecto).filter_by(id=session['pry']).first()
-#    f = db_session.query(MiembrosComite).filter_by(nro_orden=request.args.get('nro')).filter_by(id_proyecto=pro.id).first()  
-#    form = MiembrosComiteFormulario(request.form,f)
-#    miembrosComite = db_session.query(MiembrosComite).filter_by(nro_orden=form.nro_orden.data).filter_by(id_proyecto=pro.id).first()  
-#    form.id_proyecto.data = pro.nombre
-#    if pro.estado != 'N' :
-#        flash('No se pueden modificar MiembrosComites del Proyecto','info')
-#        return render_template('miembrosComite/administrarmiembrosComite.html') 
-#    if request.method == 'POST' and form.validate():
-#        if form.fecha_inicio.data > form.fecha_fin.data :
-#            flash('La fecha de inicio no puede ser mayor que la fecha de finalizacion','error')
-#            return render_template('miembrosComite/editarmiembrosComite.html', form=form) 
-#        try:
-#            form.populate_obj(miembrosComite)
-#            miembrosComite.id_proyecto = pro.id
-#            db_session.merge(miembrosComite)
-#            db_session.commit()
-#            return redirect('/miembrosComite/administrarmiembrosComite')
-#        except DatabaseError, e:
-#            flash('Error en la Base de Datos' + e.args[0],'error')
-#            return render_template('miembrosComite/editarmiembrosComite.html', form=form)
-#    else:
-#        flash_errors(form) 
-#    return render_template('miembrosComite/editarmiembrosComite.html', form=form)
-
 @app.route('/miembrosComite/eliminarmiembrosComite', methods=['GET', 'POST'])
 def eliminarmiembrosComite():
     init_db(db_session)
@@ -89,9 +61,8 @@ def eliminarmiembrosComite():
         flash('No se pueden desasignar Miembros al Comites de Cambios','info')
         return render_template('miembrosComite/administrarmiembrosComite.html') 
     try:
-        mi = request.args.get('id')
         init_db(db_session)
-        miembrosComite = db_session.query(MiembrosComite).filter_by(mi=id).first()  
+        miembrosComite = db_session.query(MiembrosComite).filter_by(id=request.args.get('id_mc')).first()  
         init_db(db_session)
         db_session.delete(miembrosComite)
         db_session.commit()
@@ -120,8 +91,7 @@ def administrarmiembrosComite():
 @app.route('/miembrosComite/listarusuarios')
 def listarusuarios():
     init_db(db_session)
-    #usuarios = db_session.query(Usuario).order_by(Usuario.nombre)
-    usuarios = db_session.query(Usuario).join(MiembrosComite, Usuario.id == MiembrosComite.id_usuario).filter(MiembrosComite.id_proyecto == session['pry'])
+    usuarios = db_session.query(Usuario).from_statement("select * from usuario where id not in (select id_usuario from miembros_comite where id_proyecto='"+session['pry'].__repr__()+"')").all()
     return render_template('miembrosComite/listarusuarios.html', usuarios = usuarios)
     
 """Lanza un mensaje de error en caso de que la pagina solicitada no exista"""
