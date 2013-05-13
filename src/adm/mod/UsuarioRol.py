275c4976e6ffa6517b29a1aedad5b5d696c075ec
lila@lila.com <git@git.com>
2013-05-12 20:00:58 -0400

    Cambios en proyecto

--- src/adm/mod/UsuarioRol.py
+++ src/adm/mod/UsuarioRol.py
@@ -7,19 +7,18 @@ from adm.mod.Usuario import Usuario
 class UsuarioRol (Base):
     __tablename__ = 'usuario_rol'
     id = Column('id', Integer, Sequence('usuario_rol_id_seq'), primary_key=True)
-    nombre = Column('nombre', String(50))
-    descripcion = Column('descripcion', String(100))
     id_rol = Column(Integer, ForeignKey('rol.id'))
     rol = relationship('Rol', backref=backref('rolesusuarios', lazy='dynamic'))
     id_usuario = Column(Integer, ForeignKey('usuario.id'))
     usuario = relationship('Usuario', backref=backref('usuariosroles', lazy='dynamic'))
+    id_proyecto = Column(Integer, ForeignKey('proyecto.id'))
+    proyecto = relationship('Proyecto', backref=backref('proyectosroles', lazy='dynamic'))
     
-    def __init__(self, nombre=None, descripcion=None, id_rol=None, id_usuario=None):
-        self.nombre = nombre
-        self.descripcion = descripcion
+    def __init__(self, id_rol=None, id_usuario=None, id_proyecto=None):
         self.id_rol = id_rol
         self.id_usuario = id_usuario
+        self.id_proyecto = id_proyecto
     
     def __repr__(self):
-        return '<Usuario Rol %s %s %s %s>' % (self.nombre, self.descripcion, self.rol, self.id_usuario)
+        return '<Usuario Rol %s %s %s %s>' % (self.id_rol, self.id_usuario, self.id_proyecto)
     
