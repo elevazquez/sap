b805e473484c567c70a45e31a67d6a659552484f
elena <mariaelena.vazquez@gmail.com>
2013-05-19 02:41:09 -0400

    cambios en permiso

--- src/UserPermission.py
+++ src/UserPermission.py
@@ -1,24 +1,21 @@
 from loginC import app
 from collections import namedtuple
-from functools import partial
-
-from flask_login import current_user
-from flask_principal import Permission, RoleNeed
+from flask_principal import Permission, ItemNeed
 from util.database import engine
 from sqlalchemy.orm import scoped_session, sessionmaker
 
 #crea un subtipo de tupla llamado permiso_edit,cuyo nombre de campos son method y value
-PermisoNeed = namedtuple('permiso_edit', ['method', 'value'])
+RecursoNeed = namedtuple('permiso_edit', ['method', 'value'])
 #crea un objeto partial, que cuando se llama va a comportarse como "PermisoNeed" llamada con los argumentos posicionales 'edit'
-EditPermisoNeed = partial(PermisoNeed, 'edit')
+#ManageRecursoNeed = partial(PermisoNeed, 'edit')
 
 db_session = scoped_session(sessionmaker(autocommit=False,
                                          autoflush=False,
                                          bind=engine))
 
 class UserPermission(Permission):
-    def __init__(self, rol):
-        need = RoleNeed(unicode(rol))
+    def __init__(self, action, idr):
+        need = ItemNeed(action, idr, 'manage')
         super(UserPermission, self).__init__(need)
 
     # Assuming the User model has a list of posts the user
