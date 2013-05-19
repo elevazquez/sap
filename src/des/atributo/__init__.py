0386d339d5112fe76834707f1024f42d48902ef8
raquel <raduac88@gmail.com>
2013-05-19 11:30:33 -0400

    modificaciones en atributo y tipo atributo

--- src/des/atributo/__init__.py
+++ src/des/atributo/__init__.py
@@ -7,6 +7,7 @@ from flask import Flask, render_template, request, redirect, url_for, flash
 from des.mod.Atributo import Atributo
 from des.mod.TipoAtributo import TipoAtributo
 from des.atributo.AtributoFormulario import AtributoFormulario
+from des.atributo.AtributoEdFormulario import AtributoEdFormulario
 import flask, flask.views
 import os
 
@@ -25,16 +26,17 @@ def flash_errors(form):
                 error
             ),'error')
                 
-""" Funcion para agregar registros a la tabla ATributos""" 
+
 @app.route('/atributo/nuevoatributo', methods=['GET', 'POST'])
 def nuevoatributo():
+        """ Funcion para agregar registros a la tabla ATributos""" 
         form = AtributoFormulario(request.form)
         init_db(db_session)
         form.id_tipo_atributo.choices= [(t.id, t.nombre) for t in db_session.query(TipoAtributo).order_by(TipoAtributo.nombre).all()]
         if request.method == 'POST' and form.validate():
             init_db(db_session)
             try: 
-                att = Atributo( form.codigo.data, form.nombre.data, form.descripcion.data, form.id_tipo_atributo.data)
+                att = Atributo(form.nombre.data, form.descripcion.data, form.id_tipo_atributo.data)
                 db_session.add(att)
                 db_session.commit()
                 flash('El Atributo ha sido registrado con exito','info')
@@ -49,12 +51,19 @@ def nuevoatributo():
 
 @app.route('/atributo/editaratributo', methods=['GET', 'POST'])
 def editaratributo():
-    form = AtributoFormulario(request.form)
+    """funcion que sirve para modificar atributos"""
     init_db(db_session)
-    atributo = db_session.query(Atributo).filter_by(codigo=form.codigo.data).first()  
+    a = db_session.query(Atributo).filter_by(nombre=request.args.get('nom')).first()  
+    form = AtributoEdFormulario(request.form,a)
+    tipo_selected= db_session.query(TipoAtributo).filter_by(nombre=request.args.get('tipo_atributo')).first() 
+    atributo = db_session.query(Atributo).filter_by(nombre=form.nombre.data).first()  
+    if request.method != 'POST':
+        form.id_tipo_atributo.data= tipo_selected.nombre
     if request.method == 'POST' and form.validate():
         try:
+            tipo=db_session.query(TipoAtributo).filter_by(nombre=form.id_tipo_atributo.data).first() 
             form.populate_obj(atributo)
+            atributo.id_tipo_atributo = tipo.id
             db_session.merge(atributo)
             db_session.commit()
             return redirect('/atributo/administraratributo')
@@ -63,17 +72,17 @@ def editaratributo():
             return render_template('atributo/editaratributo.html', form=form)
     else:
         flash_errors(form)
-        return render_template('atributo/editaratributo.html', form=form)
+    return render_template('atributo/editaratributo.html', form=form)
 
-
-
+    
 
 @app.route('/atributo/eliminaratributo', methods=['GET', 'POST'])
 def eliminaratributo():
+    """funcion que elimina un atributo"""
     try:
-        cod = request.args.get('codigo')
+        cod = request.args.get('nom')
         init_db(db_session)
-        atributo = db_session.query(Atributo).filter_by(codigo=cod).first()
+        atributo = db_session.query(Atributo).filter_by(nombre=cod).first()
         init_db(db_session)
         db_session.delete(atributo)
         db_session.commit()
@@ -84,6 +93,7 @@ def eliminaratributo():
     
 @app.route('/atributo/buscaratributo', methods=['GET', 'POST'])
 def buscaratributo():
+    """funcion que permite buscar un atributos"""
     valor = request.args['patron']
     parametro = request.args['parametro']
     init_db(db_session)
@@ -105,17 +115,20 @@ def buscaratributo():
 
 @app.route('/atributo/administraratributo')
 def administraratributo():
+    """funcion que lista todos los atributos"""
     init_db(db_session)
     atributos = db_session.query(Atributo).order_by(Atributo.nombre)
     return render_template('atributo/administraratributo.html', atributos = atributos)
 
-"""Lanza un mensaje de error en caso de que la pagina solicitada no exista"""
+
 @app.errorhandler(404)
 def page_not_found(error):
+    """Lanza un mensaje de error en caso de que la pagina solicitada no exista"""
     return 'Esta Pagina no existe', 404
 
-"""Cierra la sesion de la conexion con la base de datos"""
+
 @app.after_request
 def shutdown_session(response):
+    """Cierra la sesion de la conexion con la base de datos"""
     db_session.remove()
     return response
--- src/des/mod/TipoAtributo.py
+++ src/des/mod/TipoAtributo.py
@@ -6,15 +6,13 @@ class TipoAtributo (Base):
     __tablename__ = 'tipo_atributo'
     __table_args__ = {'extend_existing': True}
     id = Column('id', Integer, Sequence('tipo_atributo_id_seq'), primary_key=True)
-    codigo = Column('codigo', String(50), unique=True)
-    nombre = Column('nombre', String(50))
+    nombre = Column('nombre', String(50),unique=True)
     descripcion = Column('descripcion', String(100))
     
-    def __init__(self, codigo=None, nombre=None, descripcion=None):
-        self.codigo = codigo
+    def __init__(self,  nombre=None, descripcion=None):
         self.nombre = nombre
         self.descripcion = descripcion
         
     def __repr__(self):
-        return '<TipoAtributo %s %s %s>' % (self.codigo, self.nombre, self.descripcion)
+        return '<TipoAtributo %s %s %s>' % (self.nombre, self.descripcion)
     
