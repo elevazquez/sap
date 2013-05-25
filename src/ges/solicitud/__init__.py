1f073c3b32315a5032347d7a74502dd69e0dbe04
lila@lila.com <git@git.com>
2013-05-25 03:30:50 -0400

    actualizacion de solicitud, quitar item

--- src/ges/solicitud/__init__.py
+++ src/ges/solicitud/__init__.py
@@ -346,7 +346,7 @@ def quitaritemsol():
     #solicitud = db_session.query(SolicitudCambio).filter_by(id= sol.id).first() 
     #itemssol=  db_session.query(SolicitudItem).join(Item, SolicitudItem.id_item== Item.id).filter(SolicitudItem.id_solicitud== sol.id ).all()   
     #itemssol=  db_session.query(SolicitudItem).from_statement("select * from item where id in(select id_item from solicitud_item where id_solicitud='"+str(sol.id)+"')")
-    itemssol=  db_session.query(Item).join(SolicitudItem, SolicitudItem.id_item== Item.id).filter(SolicitudItem.id_solicitud== sol.id ).all()   
+    itemssol=  db_session.query(Item).join(SolicitudItem, Item.id== SolicitudItem.id_item).filter(SolicitudItem.id_solicitud== sol.id ).all()   
     if request.method == 'POST' and form.validate(): 
         items=request.form.getlist('selectitem')
         try:
