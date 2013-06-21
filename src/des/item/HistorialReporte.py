7e8bb2553a0e6b0296deaeac74c675da0337643e
lila@lila.com <git@git.com>
2013-06-21 19:25:34 -0400

    modificaciones en reportes

--- src/des/item/HistorialReporte.py
+++ src/des/item/HistorialReporte.py
@@ -8,16 +8,18 @@ from reportlab.lib.units import cm
 from reportlab.lib.enums import TA_CENTER, TA_JUSTIFY
 from reportlab.lib.colors import navy, yellow, red
 
-from geraldo import Report, ReportBand, Label, ObjectValue, SystemField, FIELD_ACTION_COUNT, FIELD_ACTION_SUM, BAND_WIDTH, Line, ReportGroup
+from geraldo import Report, ReportBand, landscape, Label, ObjectValue, SystemField, FIELD_ACTION_COUNT, FIELD_ACTION_SUM, BAND_WIDTH, Line, ReportGroup
 
 class HistorialReporte(Report):
     title = 'Historial de Item'
-
+    page_size = landscape(A4)
+    margin_left = 2*cm
+    
     class band_summary(ReportBand):
         height = 0.8*cm
         elements = [
             Label(text="Cantidad de Versiones de Item:", top=0.1*cm, left=0),
-            ObjectValue(attribute_name='id', top=0.1*cm, left=7*cm,\
+            ObjectValue(attribute_name='id', top=0.1*cm, left=5*cm,\
                 action=FIELD_ACTION_COUNT, display_format='%s '),
         ]
         borders = {'all': True}
@@ -27,33 +29,33 @@ class HistorialReporte(Report):
         elements = [
             SystemField(expression='%(report_title)s', top=0.1*cm, left=0, width=BAND_WIDTH,
                 style={'fontName': 'Helvetica-Bold', 'fontSize': 16, 'alignment': TA_CENTER}),
-            Label(text="Fase", top=0.8*cm, left=1),
-            Label(text="Tipo Item", top=0.8*cm, left=3*cm),
-            Label(text="Codigo", top=0.8*cm, left=5*cm),
-            Label(text="Version", top=0.8*cm, left=7*cm),
-            Label(text="Descripcion", top=0.8*cm, left=9*cm),
-            Label(text="Estado", top=0.8*cm, left=13*cm),
-            Label(text="Costo", top=0.8*cm, left=15*cm),
-            Label(text="Complejidad", top=0.8*cm, left=17*cm)
+            Label(text="Fase", top=0.8*cm, left=0*cm),
+            Label(text="Tipo Item", top=0.8*cm, left=5*cm),
+            Label(text="Codigo", top=0.8*cm, left=10*cm),
+            Label(text="Version", top=0.8*cm, left=12*cm),
+            Label(text="Descripcion", top=0.8*cm, left=15*cm),
+            Label(text="Estado", top=0.8*cm, left=19*cm),
+            Label(text="Costo", top=0.8*cm, left=21*cm),
+            Label(text="Complejidad", top=0.8*cm, left=23*cm)
         ]
         borders = {'bottom': Line(stroke_color=red, stroke_width=3)}
 
     class band_page_footer(ReportBand):
         height = 0.5*cm
         elements = [
-            Label(text='SAP Sistema de Administracion de Proyectos', top=0.1*cm),
+            Label(text='SAP - Sistema de Administracion de Proyectos', top=0.1*cm),
         ]
         borders = {'top': Line(stroke_color=navy)}
 
     class band_detail(ReportBand):
-        height = 0.7*cm
+        height = 0.9*cm
         elements = [
-            ObjectValue(attribute_name='id_fase', top=0, left=1*cm),
-            ObjectValue(attribute_name='id_tipo_item', top=0, left=3*cm),
-            ObjectValue(attribute_name='codigo', top=0, left=6*cm),
-            ObjectValue(attribute_name='version', top=0, left=8*cm),
-            ObjectValue(attribute_name='descripcion', top=0, left=9*cm),
-            ObjectValue(attribute_name='estado', top=0, left=13*cm),
-            ObjectValue(attribute_name='costo', top=0, left=15*cm),
-            ObjectValue(attribute_name='complejidad', top=0, left=17*cm)
+            ObjectValue(attribute_name='id_fase', top=0, left=0*cm),
+            ObjectValue(attribute_name='id_tipo_item', top=0, left=5*cm),
+            ObjectValue(attribute_name='codigo', top=0, left=10*cm),
+            ObjectValue(attribute_name='version', top=0, left=12*cm),
+            ObjectValue(attribute_name='descripcion', top=0, left=15*cm),
+            ObjectValue(attribute_name='estado', top=0, left=19*cm),
+            ObjectValue(attribute_name='costo', top=0, left=21*cm),
+            ObjectValue(attribute_name='complejidad', top=0, left=23*cm)
         ]
--- src/des/item/ListaReporte.py
+++ src/des/item/ListaReporte.py
@@ -8,11 +8,12 @@ from reportlab.lib.units import cm
 from reportlab.lib.enums import TA_CENTER, TA_JUSTIFY
 from reportlab.lib.colors import navy, yellow, red
 
-from geraldo import Report, ReportBand, Label, ObjectValue, SystemField, FIELD_ACTION_COUNT, FIELD_ACTION_SUM, BAND_WIDTH, Line, ReportGroup
+from geraldo import Report, landscape, ReportBand, Label, ObjectValue, SystemField, FIELD_ACTION_COUNT, FIELD_ACTION_SUM, BAND_WIDTH, Line, ReportGroup
 
 class ListaReporte(Report):
     title = 'Lista de Items'
-
+    page_size = landscape(A4)
+    margin_left = 2*cm
     class band_summary(ReportBand):
         height = 0.8*cm
         elements = [
@@ -27,10 +28,11 @@ class ListaReporte(Report):
         elements = [
             SystemField(expression='%(report_title)s', top=0.1*cm, left=0, width=BAND_WIDTH,
                 style={'fontName': 'Helvetica-Bold', 'fontSize': 14, 'alignment': TA_CENTER}),
-            Label(text="Id", top=0.8*cm, left=3*cm),
-            Label(text="Descripcion", top=0.8*cm, left=8*cm),
-            Label(text="Version", top=0.8*cm, left=12*cm),
-            Label(text="Complejidad", top=0.8*cm, left=17*cm),
+            Label(text="Id", top=0.8*cm, left=5*cm),
+            Label(text="Codigo", top=0.8*cm, left=8*cm),
+            Label(text="Descripcion", top=0.8*cm, left=13*cm),
+            Label(text="Version", top=0.8*cm, left=18*cm),
+            Label(text="Complejidad", top=0.8*cm, left=23*cm),
         ]
         borders = {'bottom': Line(stroke_color=red, stroke_width=3)}
 
@@ -44,16 +46,17 @@ class ListaReporte(Report):
     class band_detail(ReportBand):
         height = 0.7*cm
         elements = [
-            ObjectValue(attribute_name='id', top=0, left=3*cm),
-            ObjectValue(attribute_name='descripcion', top=0, left=8*cm),
-            ObjectValue(attribute_name='version', top=0, left=13*cm),
-            ObjectValue(attribute_name='complejidad', top=0, left=18*cm)
+            ObjectValue(attribute_name='id', top=0, left=5*cm),
+            ObjectValue(attribute_name='codigo', top=0, left=8*cm),
+            ObjectValue(attribute_name='descripcion', top=0, left=13*cm),
+            ObjectValue(attribute_name='version', top=0, left=18*cm),
+            ObjectValue(attribute_name='complejidad', top=0, left=23*cm)
         ]
         
     groups = [
         ReportGroup(attribute_name='id_fase',
             band_header=ReportBand(
-                height=0.7*cm,
+                height=1.5*cm,
                 elements=[
                     ObjectValue(attribute_name='id_fase', left=0, top=0.1*cm,
                         get_value=lambda instance: 'Fase: ' + (instance.nombre),
@@ -68,7 +71,7 @@ class ListaReporte(Report):
                     ObjectValue(attribute_name='id', action=FIELD_ACTION_COUNT,
                         display_format='%s ', left=5*cm, top=0.1*cm),
                 ],
-                borders={'top': True},
+                borders={'all': True},
             ),
         ),
     ]
