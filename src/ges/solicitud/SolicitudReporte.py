077c346329ea42536f9b2adee8a62b0d7babd2f2
lila@lila.com <git@git.com>
2013-06-21 10:57:22 -0400

    Modificacion reportes

--- src/ges/solicitud/SolicitudReporte.py
+++ src/ges/solicitud/SolicitudReporte.py
@@ -5,54 +5,87 @@ from ges.mod.SolicitudCambio import SolicitudCambio
 
 from reportlab.lib.pagesizes import A4
 from reportlab.lib.units import cm
-from reportlab.lib.enums import TA_CENTER, TA_RIGHT
+from reportlab.lib.enums import TA_CENTER, TA_JUSTIFY
+from reportlab.lib.colors import navy, yellow, red
 
-from geraldo import Report, ReportBand, Label, ObjectValue, SystemField,FIELD_ACTION_COUNT, BAND_WIDTH
-
+from geraldo import Report, ReportBand, Label, ObjectValue, SystemField, FIELD_ACTION_COUNT, FIELD_ACTION_SUM, BAND_WIDTH, Line, ReportGroup
 
 class SolicitudReporte(Report):
-    title = 'Lista de Solicitudes de Cambio'
-
-    class band_begin(ReportBand):
-        height = 1*cm
-        elements = [
-            Label(text='Mira estas solicitudes', top=0.1*cm,
-                left=8*cm),
-        ]
+    title = 'Solicitudes de Cambio'
 
     class band_summary(ReportBand):
-        height = 0.7*cm
+        height = 0.8*cm
         elements = [
-            Label(text="Eso es todo", top=0.1*cm, left=0),
-            ObjectValue(attribute_name='descripcion', top=0.1*cm, left=3*cm,
-                action=FIELD_ACTION_COUNT,
-                display_format='%s solicitudes encontradas'),
+            Label(text="Users count:", top=0.1*cm, left=0),
+            ObjectValue(attribute_name='id', top=0.1*cm, left=4*cm,\
+                action=FIELD_ACTION_COUNT, display_format='%s users found'),
         ]
         borders = {'all': True}
 
     class band_page_header(ReportBand):
         height = 1.3*cm
         elements = [
-            SystemField(expression='%(report_title)s', top=0.1*cm,
-                left=0, width=BAND_WIDTH, style={'fontName': 'Helvetica-Bold',
-                'fontSize': 14, 'alignment': TA_CENTER}),
-            Label(text="ID", top=0.8*cm, left=0),
+            SystemField(expression='%(report_title)s', top=0.1*cm, left=0, width=BAND_WIDTH,
+                style={'fontName': 'Helvetica-Bold', 'fontSize': 14, 'alignment': TA_CENTER}),
+            Label(text="ID usu", top=0.8*cm, left=0),
             Label(text="Descripcion", top=0.8*cm, left=3*cm),
+            Label(text="Estado", top=0.8*cm, left=8*cm)
         ]
-        borders = {'bottom': True}
+        borders = {'bottom': Line(stroke_color=red, stroke_width=3)}
 
     class band_page_footer(ReportBand):
         height = 0.5*cm
         elements = [
-            Label(text='Pie de pagina', top=0.1*cm, left=0),
-            SystemField(expression='Page # %(page_number)d of %(page_count)d', top=0.1*cm,
-                width=BAND_WIDTH, style={'alignment': TA_RIGHT}),
+            Label(text='SAP Sistema de Administracion de Proyectos', top=0.1*cm),
         ]
-        borders = {'top': True}
+        borders = {'top': Line(stroke_color=navy)}
 
     class band_detail(ReportBand):
-        height = 0.5*cm
+        height = 0.7*cm
         elements = [
-            ObjectValue(attribute_name='id', top=0, left=0),
-            ObjectValue(attribute_name='descripcion', top=0, left=3*cm, width=7*cm),
+            ObjectValue(attribute_name='id_usuario', top=0, left=1*cm),
+            ObjectValue(attribute_name='descripcion', top=0, left=3*cm),
+            ObjectValue(attribute_name='estado', top=0, left=13*cm)
         ]
+
+#    groups = [
+#        ReportGroup(attribute_name='is_superuser',
+#            band_header=ReportBand(
+#                height=0.7*cm,
+#                elements=[
+#                    ObjectValue(attribute_name='is_superuser', left=0, top=0.1*cm,
+#                        get_value=lambda instance: 'Superuser: ' + (instance.is_superuser and 'Yes' or 'No'),
+#                        style={'fontName': 'Helvetica-Bold', 'fontSize': 12})
+#                ],
+#                borders={'bottom': True},
+#            ),
+#            band_footer=ReportBand(
+#                height=0.7*cm,
+#                elements=[
+#                    ObjectValue(attribute_name='id', action=FIELD_ACTION_COUNT,
+#                        display_format='%s superusers', left=0*cm, top=0.1*cm),
+#                    ObjectValue(attribute_name='id', action=FIELD_ACTION_SUM,
+#                        display_format='%s is the sum of IDs above', left=4*cm, top=0.1*cm),
+#                ],
+#                borders={'top': True},
+#            ),
+#        ),
+#        ReportGroup(attribute_name='is_staff',
+#            band_header=ReportBand(
+#                height=0.7*cm,
+#                elements=[
+#                    ObjectValue(attribute_name='is_staff', left=0.5*cm, top=0.1*cm,
+#                        get_value=lambda instance: 'Staff: ' + (instance.is_staff and 'Yes' or 'No'))
+#                ],
+#                borders={'bottom': True},
+#            ),
+#            band_footer=ReportBand(
+#                height=0.7*cm,
+#                elements=[
+#                    ObjectValue(attribute_name='id', action=FIELD_ACTION_COUNT,
+#                        display_format='%s staffs', left=0.5*cm, top=0.1*cm)
+#                ],
+#                borders={'top': True},
+#            ),
+#        ),
+#    ]
--- src/ges/solicitud/__init__.py
+++ src/ges/solicitud/__init__.py
@@ -15,9 +15,14 @@ from ges.solicitud.SolicitudFormulario import SolicitudFormulario
 from ges.solicitud.ReporteFormulario import ReporteFormulario
 from ges.solicitud.SolicitudReporte import SolicitudReporte
 from flask_login import current_user
+from flask import Response 
 import flask, flask.views
 import os
 import datetime
+from des.item.ReporteHistorialFormulario import ReporteHistorialFormulario
+from des.item.HistorialReporte import HistorialReporte
+from des.item.ListaItemFormulario import ListaItemFormulario
+from des.item.ListaReporte import ListaReporte
 from geraldo.generators import PDFGenerator
 
 cur_dir = os.path.dirname(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
@@ -423,15 +428,56 @@ def reportesol():
     today = datetime.date.today()
     form = ReporteFormulario(request.form)
     form.fecha.data = today
-    #request.method == 'POST' and 
-    if  form.validate():
+    if  request.method == 'POST' and form.validate():
         reporte = SolicitudReporte(queryset=db_session.query(SolicitudCambio).filter_by(id_proyecto=session['pry']))
         reporte.generate_by(PDFGenerator, filename=os.path.join(cur_dir, cur_dir + '/static/reportes/Solicitudes.pdf'))
-        if (reporte != None) :
-            return render_template('solicitud/solicitud.html')
+        return render_template('solicitud/solicitud.html')
     else:
         flash_errors(form) 
     return render_template('solicitud/reportesolicitud.html', form=form)
+
+@app.route('/solicitud/reportehistorial', methods=['GET', 'POST'])
+def reportehistorial():   
+    """ Funcion que imprime el reporte del historial """
+    form = ReporteHistorialFormulario(request.form)
+    items = db_session.query(Item).from_statement("Select it.*  from item it, "+ 
+                        " (Select  i.codigo cod, max(i.version) vermax from item i, fase f  where i.id_fase = f.id "+
+                        " and f.id_proyecto = "+str(session['pry'])+ " group by codigo order by 1 ) s "+
+                        " where it.codigo = cod and it.version= vermax order by it.codigo ")
+    if  request.method == 'POST' and form.validate():
+        multiselect= request.form.getlist('selectitem')  
+        list_aux=[]
+        for it in multiselect : 
+            i = db_session.query(Item).filter_by(id=it).first()    
+            list_aux.append(i)
+        if list_aux == None or list_aux == []:
+            flash('Debe seleccionar un item','info')
+            return render_template('solicitud/administrarsolicitud.html')
+        i = list_aux[0]
+        reporte = HistorialReporte(queryset=db_session.query(Item).from_statement("select i.id, f.descripcion as id_fase, ti.descripcion as id_tipo_item, " +
+        " i.codigo, i.version, i.descripcion, i.estado, i.costo, i.complejidad from item i, fase f, tipo_item ti where i.codigo = '"+ str(i.codigo) +
+        "' and f.id = i.id_fase and ti.id = i.id_tipo_item order by version "))
+        reporte.generate_by(PDFGenerator, filename=os.path.join(cur_dir, cur_dir + '/static/reportes/HistorialItem.pdf'))
+        return render_template('item/historial.html')
+    else:
+        flash_errors(form) 
+    return render_template('item/reportehistorial.html', form=form, items=items)
+
+@app.route('/solicitud/reportelista', methods=['GET', 'POST'])
+def reportelista():   
+    """ Funcion que imprime el reporte la lista de items """
+    today = datetime.date.today()
+    form = ListaItemFormulario(request.form)
+    form.fecha.data = today
+    if  request.method == 'POST' and form.validate():
+        reporte = ListaReporte(queryset=db_session.query(Item).from_statement("Select it.id_fase, fa.descripcion as nombre, it.id, it.descripcion, it.version, it.complejidad from item it, fase fa," +  
+        " (Select  i.codigo cod, max(i.version) vermax from item i, fase f  where i.id_fase = f.id and f.id_proyecto = "+str(session['pry'])+ " group by codigo order by 1 ) s " +
+        " where it.codigo = cod and it.version= vermax and it.id_fase = fa.id order by it.id_fase, it.codigo "))
+        reporte.generate_by(PDFGenerator, filename=os.path.join(cur_dir, cur_dir + '/static/reportes/ListaItem.pdf'))
+        return render_template('item/ritem.html')
+    else:
+        flash_errors(form) 
+    return render_template('item/reporteitem.html', form=form)
 
 @app.route('/solicitud/administrarreportes', methods=['GET', 'POST'])
 def administrarreportes():   
